f8e4023a0d9ef7f89eeebeaf527a72e61287880b
zaramgithub <zaramagu15@gmail.com>
2023-03-15 22:34:15 -0300

    connect to mysql

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const mysql = require('mysql');
+const myconn = require('express-myconnection');
+
 
 const app = express();
-
 app.set('port',process.env.port || 9000);
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'programacionvisual22',
+    database: 'library'
+};
 
+// midlewares -----------------------------
+app.use(myconn(mysql,dbOptions,'single'));
+//routes -----------------------------------
 app.get('/',(req,res) => {
-    res.send('Welcome to my API');
+    res.send('Welcome to my API!');
 });
-
+// server running ---------------------------
 app.listen(app.get('port'), () =>{
     console.log(`server running on PORT`,app.get('port'));
 });
